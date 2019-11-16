9c16ef6fe9b9b5f1cbbc0c40824834c430606c1c
Norbert Szulc <norbert@not7cd.net>
2019-11-16 20:24:44 +0100

    Weh

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
 import os
-from flask import Flask
+import re
+from datetime import datetime
+try:
+    from urllib.parse import urljoin
+except ImportError:
+    from urlparse import urljoin
 
-TEMPLATE = """
+import requests
+from flask import Flask, request, url_for, abort, redirect, render_template
 
+email_re = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
 
+app = Flask("slack-invite")
 
-"""
+def requester_ip():
+    if request.headers.getlist("X-Forwarded-For"):
+        ip_addr = request.headers.getlist("X-Forwarded-For")[0]
+        app.logger.info(
+            "forward from %s to %s",
+            request.remote_addr,
+            request.headers.getlist("X-Forwarded-For")[0],
+        )
+    else:
+        ip_addr = request.remote_addr
+    return ip_addr
 
 class SlackInvitationException(BaseException):
     pass
 
+
 class SlackInvitationClient(object):
-    BASE_URL = 'https://{}.slack.com'
-    ENDPOINT = '/api/users.admin.invite'
+    BASE_URL = "https://{}.slack.com"
+    ENDPOINT = "/api/users.admin.invite"
 
     def __init__(self, team, token):
         self.token = token
@@ -24,24 +43,49 @@ class SlackInvitationClient(object):
 
     def invite(self, email, active=True):
         endpoint = urljoin(self.base_url, self.ENDPOINT)
-        r = requests.post(endpoint, data={
-            'email': email,
-            'token': self.token,
-            'set_active': active
-        })
+        r = requests.post(
+            endpoint, data={"email": email, "token": self.token, "set_active": active}
+        )
         response_object = r.json()
-        if r.status_code == 200 and response_object['ok']:
+        if r.status_code == 200 and response_object["ok"]:
             return True
         else:
-            raise SlackInvitationException(response_object['error'])
+            raise SlackInvitationException(response_object["error"])
 
-TOKEN = os.get("SLACK_TOKEN")
-WORKSPACE = os.get("SLACK_WORKSPACE")
+last_invite = datetime.now()
+MIN_SECONDS = 30
+
+TOKEN = os.environ.get("SLACK_TOKEN")
+WORKSPACE = os.environ.get("SLACK_WORKSPACE")
+
+if TOKEN is None or WORKSPACE is None:
+    raise Exception("Setup envs SLACK_TOKEN and SLACK_WORKSPACE")
 
 slack_client = SlackInvitationClient(WORKSPACE, TOKEN)
 
-@app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
 
+@app.route("/", methods=["GET", "POST"])
+def hello():
+    if request.method == "POST":
+        # anti abuse
+        #global last_invite
+        #seconds_elapsed = (datetime.now() - last_invite).total_seconds()
+        #if seconds_elapsed < MIN_SECONDS:
+        #    app.logger.error("Invite sent while locked, %i seconds elapsed from %i", seconds_elapsed, MIN_SECONDS)
+        #    abort(423)
+
+        if not email_re.match(request.form["email"]):
+            abort(400)
+
+        try:
+            app.logger.info("Inviting %s from %s to workspace", request.form["email"], requester_ip())
+            slack_client.invite(request.form["email"])
+        except SlackInvitationException as exc:
+            app.logger.error(exc)
+            abort(502)
+
+        last_invite = datetime.now()
+        redirect(url_for("/"))
+
+    else:
+        return render_template("index.html")
